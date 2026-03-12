ad2296b725a5835368a4115550fbe69e9ba3a69b
Ehtisham-04 <ehtisham.offcial@gmail.com>
2026-03-12 09:55:45 -0700

    ADD src folder and java files

---
+++ src/VariablesDemo.java
@@ -0,0 +1,18 @@
+public class VariablesDemo {
+
+    public static void main(String[] args) {
+
+        int age = 25;
+        double salary = 55000.50;
+        char grade = 'A';
+        boolean isActive = true;
+        String name = "Anum Bahar";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Grade: " + grade);
+        System.out.println("Active: " + isActive);
+
+    }
+}
